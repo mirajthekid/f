d70377a6e6cdfba22a4795656231cc7d93d0bce3
mirajthekid <48984210+mirajthekid@users.noreply.github.com>
2025-06-20 00:16:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,2 @@
-import React, { useState } from 'react';
-import './App.css';
-import UsernameScreen from './components/UsernameScreen';
-import ReactionGame from './components/ReactionGame';
-import OddOneOutGame from './components/OddOneOutGame';
-import MemoryGame from './components/MemoryGame';
-import DirectionGame from './components/DirectionGame';
 import AppBackup from './AppBackup';
-
 export default AppBackup;
